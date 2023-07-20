0ca0eb2e0600350824ed3177f18dd30d967b9d2b
lukasjoho <social@lukashoppe.com>
2023-07-20 03:09:48 +0200

    Remove seed from header

--- components/inventory/InventoryHeader.tsx
+++ components/inventory/InventoryHeader.tsx
@@ -5,21 +5,16 @@ import { PlusCircle } from "lucide-react";
 import SeedButton from "./SeedButton";
 import { getProducts } from "@/lib/db";
 
-const InventoryHeader = async () => {
-  const products = await getProducts();
-  const count = products.length;
+const InventoryHeader = () => {
   return (
     <div className="flex justify-between w-full mb-8">
       <h1 className="text-2xl font-semibold">Inventory</h1>
-      <div className="flex gap-2">
-        {count == 0 && <SeedButton />}
-        <OpenProductDialog>
-          <Button className="flex gap-1.5">
-            <PlusCircle className="w-4 h-4" />
-            Create Product
-          </Button>
-        </OpenProductDialog>
-      </div>
+      <OpenProductDialog>
+        <Button className="flex gap-1.5">
+          <PlusCircle className="w-4 h-4" />
+          Create Product
+        </Button>
+      </OpenProductDialog>
     </div>
   );
 };
